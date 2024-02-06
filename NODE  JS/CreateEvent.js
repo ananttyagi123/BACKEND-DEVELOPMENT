fe980adaa5bea993c03dc2b5ad2dcd3f934ab61e
Anant Tyagi <ananttyagi359@gmail.com>
2024-02-06 12:31:35 +0530

    Update events

--- NODE  JS/CreateEvent.js
+++ NODE  JS/CreateEvent.js
@@ -1,14 +1,23 @@
 var event = require('events');
 var obj = new event.EventEmitter;
 
-obj.setMaxListeners(2);
+obj.setMaxListeners(3);
 console.log(obj.getMaxListeners());
 
 // creating a loop 
-for(i=0; i<=3; i++){
-  obj.addListener("dancing",function(msg){
+for (i = 0; i <= 3; i++) {
+  obj.addListener("dancing", function (msg) {
     console.log(msg);
   })
 }
 
-obj.emit("dancing","lets Dance");
+obj.emit("dancing", "lets Dance");
+
+function Welcome(){
+  console.log("Hello Javascript!")
+}
+// Set time  Out
+var m = setTimeout(Welcome,1000);
+
+
+var n = setInterval(Welcome,2000);
